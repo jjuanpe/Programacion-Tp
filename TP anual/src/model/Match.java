826ae4d49496d691cfb07672372d76185dc7489c
Santiago <santiagodesre@gmail.com>
2026-08-11 18:01:02 -0300

    Paritdo

--- TP anual/src/model/Match.java
+++ TP anual/src/model/Match.java
@@ -4,11 +4,93 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Match {
-    private LocalDate date;
-    private Team homeTeam;
-    private Team awayTeam;
-    private Referee referee;
-    private Stadium stadium;
-    private List<Incidence> incidents;
+    import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+    public abstract class Match {
+
+        private LocalDate date;
+        private Team homeTeam;
+        private Team awayTeam;
+        private Referee referee;
+        private Stadium stadium;
+        private int homeGoals;
+        private int awayGoals;
+        private Formation homeFormation;
+        private Formation awayFormation;
+        private List<PlayerParticipation> participations;
+        private List<Incidence> incidences;
+
+        public Match(LocalDate date, Team homeTeam, Team awayTeam, Referee referee, Stadium stadium) {
+            this.date = date;
+            this.homeTeam = homeTeam;
+            this.awayTeam = awayTeam;
+            this.referee = referee;
+            this.stadium = stadium;
+            this.participations = new ArrayList<>();
+            this.incidences = new ArrayList<>();
+        }
+
+        public LocalDate getDate() {
+            return date;
+        }
+
+
+        public Team getHomeTeam() {
+            return homeTeam;
+        }
+
+
+        public Team getAwayTeam() {
+            return awayTeam;
+        }
+
+
+        public Referee getReferee() {
+            return referee;
+        }
+
+
+        public Stadium getStadium() {
+            return stadium;
+        }
+
+
+        public int getHomeGoals() {
+            return homeGoals;
+        }
+
+        public int getAwayGoals() {
+            return awayGoals;
+        }
+
+
+        public Formation getHomeFormation() {
+            return homeFormation;
+        }
+
+
+        public Formation getAwayFormation() {
+            return awayFormation;
+        }
+
+
+        public List<PlayerParticipation> getParticipations() {
+            return participations;
+        }
+
+        public void setParticipations(List<PlayerParticipation> participations) {
+            this.participations = participations;
+        }
+
+        public List<Incidence> getIncidences() {
+            return incidences;
+        }
+
+        public void setIncidences(List<Incidence> incidences) {
+            this.incidences = incidences;
+        }
+
+    }
 }
