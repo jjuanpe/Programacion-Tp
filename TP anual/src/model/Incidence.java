722032e1b38ecfdd7d799b64cca1a8dabbe2d51b
tomasserrano23 <tomasserrano05@gmail.com>
2026-08-12 11:39:15 -0300

    Update Incidence.java

--- TP anual/src/model/Incidence.java
+++ TP anual/src/model/Incidence.java
@@ -1,4 +1,27 @@
 package model;
 
 public abstract class Incidence {
+    private int minute;
+    private Match match;
+
+    public Incident(int minute, Match match) {
+        this.minute = minute;
+        this.match = match;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public Match getMatch() {
+        return match;
+    }
+
+    public void setMatch(Match match) {
+        this.match = match;
+    }
 }
