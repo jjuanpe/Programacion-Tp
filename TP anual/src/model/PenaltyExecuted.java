3a60387f013bb97806e8acb45b9ff2dcf7323306
tomasserrano23 <tomasserrano05@gmail.com>
2026-08-12 11:43:31 -0300

    Update PenaltyExecuted.java

--- TP anual/src/model/PenaltyExecuted.java
+++ TP anual/src/model/PenaltyExecuted.java
@@ -1,4 +1,28 @@
 package model;
 
 public class PenaltyExecuted extends Incidence{
+    private Player kicker;
+    private boolean scored;
+
+    public Penalty(int minute, Match match, Player kicker, boolean scored) {
+        super(minute, match);
+        this.kicker = kicker;
+        this.scored = scored;
+    }
+
+    public Player getKicker() {
+        return kicker;
+    }
+
+    public void setKicker(Player kicker) {
+        this.kicker = kicker;
+    }
+
+    public boolean isScored() {
+        return scored;
+    }
+
+    public void setScored(boolean scored) {
+        this.scored = scored;
+    }
 }
