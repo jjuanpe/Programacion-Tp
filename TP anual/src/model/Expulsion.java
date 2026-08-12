c7a8154b0d1d0955ae1852eb65192a8a5906072d
tomasserrano23 <tomasserrano05@gmail.com>
2026-08-12 11:49:03 -0300

    Update Expulsion.java

--- TP anual/src/model/Expulsion.java
+++ TP anual/src/model/Expulsion.java
@@ -1,4 +1,19 @@
 package model;
 
 public class Expulsion extends Incidence{
+  
+    private Player player;
+
+    public Expulsion(int minute, Match match, Player player) {
+        super(minute, match);
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 }
