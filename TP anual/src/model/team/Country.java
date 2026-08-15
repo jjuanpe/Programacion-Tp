363ddd79b5e9c7e4ccc6378621f52843b37063e9
juanp <juanpeazcueta@gmail.com>
2026-08-15 13:07:19 -0300

    modificacion Country

--- TP anual/src/model/team/Country.java
+++ TP anual/src/model/team/Country.java
@@ -1,4 +1,13 @@
 package model.team;
 
 public class Country {
+    private String countryName;
+
+    public Country (String countryName){
+        this.countryName = countryName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
 }
