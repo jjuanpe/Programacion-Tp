efe11ee04c2e0e87c618ee9dd4ee9267332232c6
tomasserrano23 <tomasserrano05@gmail.com>
2026-08-16 20:50:04 -0300

    Update PlayerParticipation.java

--- TP anual/src/model/match/PlayerParticipation.java
+++ TP anual/src/model/match/PlayerParticipation.java
@@ -1,4 +1,28 @@
 package model.match;
 
+import model.people.Player;
+
 public class PlayerParticipation {
+    private final Player player;
+    private final boolean starter;
+    private int minutesPlayed;
+
+    public PlayerParticipation(Player player, boolean starter) {
+        this.player = player;
+        this.starter = starter;
+        this.minutesPlayed = 0;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public boolean isStarter() {
+        return starter;
+    }
+
+    public int getMinutesPlayed() {
+        return minutesPlayed;
+    }
+
 }
